11344cb7a792bc53192e74f9b366798a5918e985
Jean Boussier <jean.boussier@gmail.com>
2022-07-29 12:20:05 +0200

    WIP
    ```
    Warming up --------------------------------------
                   erubi    61.247k i/100ms
                   phlex     4.921k i/100ms
    Calculating -------------------------------------
                   erubi    612.755k (± 0.4%) i/s -      3.124M in   5.097709s
                   phlex     48.894k (± 0.4%) i/s -    246.050k in   5.032430s
    
    Comparison:
                   erubi:   612755.2 i/s
                   phlex:    48893.8 i/s - 12.53x  (± 0.00) slower
    ```

--- bench/nav.rb
+++ bench/nav.rb
@@ -1,22 +1,63 @@
 #!/usr/bin/env ruby
 
 require "phlex"
+require "erubi"
 require "benchmark/ips"
 
 class NavComponent < Phlex::Component
+  def initialize(links)
+    @links = links
+  end
+
   def template
     nav id: "main_nav" do
       ul do
-        li { a "Home", href: "/" }
-        li { a "About", href: "/about" }
-        li { a "Contact", href: "/contact" }
+        @links.each do |title, url|
+          li { a title, href: url }
+        end
       end
     end
   end
 end
 
+links = {
+  "Home" => "/",
+  "About" => "/about",
+  "Contact" => "/contact",
+}
+
+class NavTemplate
+  src = Erubi::Engine.new(<<~HTML, escape: true).src
+    <nav id="main_nav">
+      <ul>
+        <% @links.each do |title, url| %>
+          <li><a href="<%= url %>"><%= title %></a></li>
+        <% end %>
+      </ul>
+    </nav>
+  HTML
+
+  def initialize(links)
+    @links = links
+  end
+
+  class_eval <<~RUBY, __FILE__, __LINE__ + 1
+    def call
+      #{src}
+    end
+  RUBY
+end
+
 puts RUBY_DESCRIPTION
+puts '-' * 40
+puts NavTemplate.new(links).call
+puts '-' * 40
+puts NavComponent.new(links).call
+puts '-' * 40
+
 
 Benchmark.ips do |x|
-  x.report("NavComponent") { NavComponent.new.call }
+  x.report("erubi") { NavTemplate.new(links).call }
+  x.report("phlex") { NavComponent.new(links).call }
+  x.compare!
 end
